109eb6ab435ad1907e0d4cd881edbf978ee06d96
[vulturechoujin] <E1528675@u.nus.edu>
2025-12-07 16:48:01 +0800

    First commit

---
+++ src/Search.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Search(){
+    return(
+        <section className = "searchbox-wrap">
+            <input type = "text" placeholder = "Search for a movie..." classname = "searchbox"></input>
+        </section>
+    )
+}
+
+export default Search;
